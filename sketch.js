3135ad31e3a2464309db3284792c371609b61276
rishku <94534351+rishku@users.noreply.github.com>
2022-03-09 07:49:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var zombieImg;
+var bullet;
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+  zombieImg = loadImage("assets/zombie.png")
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 1.1
+  
+
+//creating the player sprite
+player = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.3
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+if(keyWentDown("space")){
+ 
+  player.addImage(shooter_shooting)
+ 
+}
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyWentUp("space")){
+  player.addImage(shooterImg)
+  bullet = createSprite(player.x,player.y,10,10)
+  bullet.shapeColor = "lightblue"
+  bullet.velocityX = 3;
+  
+}
+ spawnZombie()
+drawSprites();
+
+}
+ function spawnZombie(){
+   if(frameCount % 110 === 0){
+     var zombie = createSprite(displayWidth-10,displayHeight/2+200.50,50)
+     zombie.y = Math.round(random(displayHeight/2,displayHeight/2+300));
+     zombie.addImage("walk",zombieImg);
+     zombie.scale = 0.2;
+     zombie.velocityX = -2;
+
+     zombie.lifetime = 600;
+   }
+ }
